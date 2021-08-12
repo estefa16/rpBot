499f91b3a48bda1d21edc5c09f50e1000c6fe1cf
estefa16 <iceprinzessin@hotmail.com>
2021-08-12 18:07:26 +1000

    Add files via upload

---
+++ rpBot/list.py
@@ -0,0 +1,30 @@
+
+
+###################
+## LIST FUNCTIONS #
+###################
+
+class List:
+
+	#Initialise List
+	def __init__(self, suburb_doc):
+		self.suburb_doc = suburb_doc
+		suburb_list = []
+
+	#Get list from suburb document
+	def get_list(self, suburb_doc):
+		pass
+
+	#Add suburb to list  
+	def add_to_list(self, suburb):
+		pass
+
+
+	#Remove specific suburb from list
+	def remove_from_list(self, suburb):
+		pass
+
+
+	#Update corrupted list --> Wipe list and go throguh document to create new list
+	def update_list(self, document):
+		pass
